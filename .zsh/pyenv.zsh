a84cc588fd9bb2c3de43a01f3db2d40141ad6cc2
Bernard Laveaux <blaveaux@brex.com>
2021-07-22 15:12:56 -0700

    init pyenv shims within zprofile

--- .zsh/pyenv.zsh
+++ .zsh/pyenv.zsh
@@ -1,3 +1,7 @@
+# NOTE: unnecessary when pyenv installed via Homebrew
+# export PYENV_ROOT="$HOME/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PATH"
+
 if command -v pyenv 1>/dev/null 2>&1; then
   eval "$(pyenv init -)"
 fi
