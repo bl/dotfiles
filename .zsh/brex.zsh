4bc71c4a6291aa138345bf9abb22ec014fd6564a
Bernard Laveaux <blaveaux@brex.com>
2021-10-22 16:49:44 -0700

    cleanup zsh additional configs

--- .zsh/brex.zsh
+++ .zsh/brex.zsh
@@ -1,3 +1,12 @@
 export SRC_ROOT="${HOME}/src"
 
+export BREX_DEV_NAMESPACE="$(id -un)"
+
+# init nodeenv
 eval "$(nodenv init -)"
+
+# init asdf
+. /usr/local/opt/asdf/asdf.sh
+
+# kube autocomplete
+[[ /usr/local/bin/kubectl ]] && source <(kubectl completion zsh)
